12f0d9c8c68a55fd89c11df97df55f3e73a53c83
Jhonatan Villamizar <jhonatan.avj@gmail.com>
2024-04-08 19:33:10 -0500

    Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jhonatanvillamizar.github.io',
+  base: '/Esland-web',
   integrations: [tailwind()],
   i18n: {
     defaultLocale: 'es',
